3305fa7aeb53bbb9f0b1218a343ad33501651a15
Afo Oshun <afoyenyen@Afos-iMac.local>
2020-09-06 01:30:50 +0100

    added redux persist and enabled local stirage functionalitu

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { PersistGate } from 'redux-persist/integration/react';
+
 import { Provider } from 'react-redux';
 import { ThemeProvider } from '@material-ui/styles';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 import theme from './ui/Theme';
 
 ReactDOM.render(
 	<Provider store={store}>
 		<ThemeProvider theme={theme}>
 			<BrowserRouter>
-				<App />
+				<PersistGate persistor={persistor}>
+					<App />
+				</PersistGate>{' '}
 			</BrowserRouter>{' '}
 		</ThemeProvider>{' '}
 	</Provider>,
