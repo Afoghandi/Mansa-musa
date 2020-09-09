53d4cea9897f5ddc94ede4bbadf212f12b3a4351
Afo Oshun <afoyenyen@Afos-iMac.local>
2020-09-10 00:50:22 +0100

    Updates

---
+++ precache-manifest.182b6ce902d70d4b345978bf0097b596.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc3eec0fd4a8ba62de39a2b524425b9b",
+    "url": "/Mansa-musa/index.html"
+  },
+  {
+    "revision": "a7bd8dc782d5740377e5",
+    "url": "/Mansa-musa/static/js/2.8908e79c.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/Mansa-musa/static/js/2.8908e79c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "979b8a58e4e2774788db",
+    "url": "/Mansa-musa/static/js/main.ac02b5f5.chunk.js"
+  },
+  {
+    "revision": "c5a096dd0248b8ca3a99",
+    "url": "/Mansa-musa/static/js/runtime-main.4c60ac5f.js"
+  }
+]);
